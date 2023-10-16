2fbb7b76526913bcb1621afd5a2576b8bc08470b
othiagofelippe <thiagofelippe135@gmail.com>
2023-10-16 14:04:49 -0300

    Layout Structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.tsx",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(10rem, 20rem) 1fr',
+      },
+    },
   },
   plugins: [],
-};
+}
